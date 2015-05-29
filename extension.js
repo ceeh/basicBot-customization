bfd37dbb77e4e4bd0e3c27aabf8ddbfd68cda36a
ceeh <heeh12@gmail.com>
2015-05-29 15:33:45 +0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 (function () {
     //Link location of your fork so you don't have to modify so many things.
-    var fork = "Yemasthui";
+    var fork = "ceeh";
 		
     //Define our function responsible for extending the bot.
     function extend() {
@@ -43,6 +43,19 @@
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
                     API.sendChat("/me Bacon!!!");
+                }
+            }
+        };
+        
+        bot.commands.cheeseCommand = {
+            command: 'cheese',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me Cheese!!!");
                 }
             }
         };
